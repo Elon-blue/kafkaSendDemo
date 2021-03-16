fc078490b771914b01447fc39dff6002e674d3c0
456 <456@456.com>
2021-03-16 10:10:13 +0800

    kafka controller  send

--- src/main/java/com/fan/che/kafkatemplate/controller/KafkaProducerController.java
+++ src/main/java/com/fan/che/kafkatemplate/controller/KafkaProducerController.java
@@ -1,17 +1,11 @@
 package com.fan.che.kafkatemplate.controller;
 
-import org.apache.kafka.clients.producer.Callback;
-import org.apache.kafka.clients.producer.KafkaProducer;
-import org.apache.kafka.clients.producer.ProducerRecord;
-import org.apache.kafka.clients.producer.RecordMetadata;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.UUID;
-import java.util.concurrent.ExecutionException;
 
 @RestController
 @RequestMapping("/kafkaProducer")
@@ -20,30 +14,22 @@ public class KafkaProducerController {
     private final static Logger logger = LoggerFactory.getLogger(KafkaProducerController.class);
 
 
+    @Autowired
+    private KafkaTemplate<String, String> kafkaTemplate;
 
-
-    @RequestMapping(value = "/{topic}/send",method = RequestMethod.GET)
-    public void sendMeessageTotopic2(@PathVariable String topic, @RequestParam(value = "partition",defaultValue = "0") int partition) {
-//        KafkaProducer<String, String> kafkaProducer = new KafkaProducer<String, String>();
-        logger.info("start send message to {}",topic);
-        for (int i = 0; i <=10; i++) {
+    @RequestMapping(value = "/{topic}/send", method = RequestMethod.GET)
+    public void sendMeessageTotopic2(@PathVariable String topic, @RequestParam(value = "partition", defaultValue = "0") int partition) {
+        logger.info("start send message to {}", topic);
+        for (int i = 0; i <= 10; i++) {
 
             long startTime = System.currentTimeMillis();
-            System.out.println("===startTime "+startTime);
+            System.out.println("===startTime " + startTime);
             String message = Long.toString(startTime);
-            System.out.println("start send message:"+message);
-//            kafkaProducer.send(topic,partition,"demo0315",message);
+            System.out.println("start send message:" + message);
+            kafkaTemplate.send(topic, partition, "demo0315", message);
 
         }
 
         System.out.println("send message over");
     }
-
-
-
-
-
-
-
-
 }
